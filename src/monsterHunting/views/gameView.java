7cd8e9e6939429d5a13f307fade80e71f7523e10
jimmy <zernox@gmail.com>
2017-10-04 07:56:35 +0200

    trying out images

--- src/monsterHunting/views/gameView.java
+++ src/monsterHunting/views/gameView.java
@@ -2,6 +2,7 @@ package monsterHunting.views;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.Graphics2D;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -11,16 +12,25 @@ import monsterHunting.common.Monster;
 import monsterHunting.common.Player;
 
 import javax.swing.JTextPane;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.Random;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextArea;
 import java.awt.GridBagLayout;
+import java.awt.Image;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
+import java.awt.RenderingHints;
+
 import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
+import javax.swing.JLabel;
 
 public class gameView extends JFrame {
 
@@ -33,6 +43,7 @@ public class gameView extends JFrame {
 	private Monster monsterOne;
 	private Random rnd;
 	private JScrollPane scrollPane;
+	private JLabel lblMonster;
 	/**
 	 * Launch the application.
 	 */
@@ -85,6 +96,9 @@ public class gameView extends JFrame {
 				{
 					battleMode();
 				}
+				
+				
+				
 				else
 				{
 					textArea.setText(textArea.getText() + "\n\n" + "There is no monster, you have to find someone to battle!");
@@ -112,14 +126,26 @@ public class gameView extends JFrame {
 		btnFight = new JButton("Fight");
 		
 		GridBagConstraints gbc_btnFight = new GridBagConstraints();
+		gbc_btnFight.fill = GridBagConstraints.HORIZONTAL;
 		gbc_btnFight.insets = new Insets(0, 0, 5, 5);
 		gbc_btnFight.gridx = 0;
 		gbc_btnFight.gridy = 0;
 		contentPane.add(btnFight, gbc_btnFight);
 		
+		lblMonster = new JLabel("New label");
+		GridBagConstraints gbc_lblMonster = new GridBagConstraints();
+		gbc_lblMonster.fill = GridBagConstraints.BOTH;
+		gbc_lblMonster.gridwidth = 4;
+		gbc_lblMonster.gridheight = 4;
+		gbc_lblMonster.insets = new Insets(0, 0, 5, 5);
+		gbc_lblMonster.gridx = 2;
+		gbc_lblMonster.gridy = 0;
+		contentPane.add(lblMonster, gbc_lblMonster);
+		
 		btnPotion = new JButton("Potion");
 		
 		GridBagConstraints gbc_btnPotion = new GridBagConstraints();
+		gbc_btnPotion.fill = GridBagConstraints.HORIZONTAL;
 		gbc_btnPotion.insets = new Insets(0, 0, 5, 5);
 		gbc_btnPotion.gridx = 0;
 		gbc_btnPotion.gridy = 1;
@@ -128,6 +154,7 @@ public class gameView extends JFrame {
 		btnSpawnMonster = new JButton("SpawnMonster");
 		
 		GridBagConstraints gbc_btnSpawnMonster = new GridBagConstraints();
+		gbc_btnSpawnMonster.fill = GridBagConstraints.HORIZONTAL;
 		gbc_btnSpawnMonster.insets = new Insets(0, 0, 5, 5);
 		gbc_btnSpawnMonster.gridx = 0;
 		gbc_btnSpawnMonster.gridy = 2;
@@ -135,9 +162,10 @@ public class gameView extends JFrame {
 		
 		scrollPane = new JScrollPane();
 		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+		gbc_scrollPane.gridwidth = 6;
 		gbc_scrollPane.insets = new Insets(0, 0, 0, 5);
 		gbc_scrollPane.fill = GridBagConstraints.BOTH;
-		gbc_scrollPane.gridx = 6;
+		gbc_scrollPane.gridx = 1;
 		gbc_scrollPane.gridy = 4;
 		contentPane.add(scrollPane, gbc_scrollPane);
 		
@@ -148,6 +176,12 @@ public class gameView extends JFrame {
 	public void gameStart() {
 		createPlayer();	
 		createMonster();
+		
+		Image image = new ImageIcon(this.getClass().getResource("/MonsterHunting/src/monsterHunting/resources/zombieFemale.png")).getImage();
+		ImageIcon icon = new ImageIcon(image);
+		
+		
+		lblMonster.setIcon(icon);
 	}
 	
 	public void createMonster() {
@@ -242,6 +276,23 @@ public class gameView extends JFrame {
 			
 		}
 	}
+	private void monsterImage()
+	{
+		/*BufferedImage img = null;
+		try {
+		    img = ImageIO.read(new File("zombieFemale.png"));
+		} catch (IOException e) {
+		    e.printStackTrace();
+		}
+		
+		Image dimg = img.getScaledInstance(lblMonster.getWidth(), lblMonster.getHeight(),
+		        Image.SCALE_SMOOTH);
+		
+		ImageIcon imageIcon = new ImageIcon(dimg);
+		lblMonster.setIcon(imageIcon);*/
+		
+		
+	}
 	
 	private void levelUp()
 	{
@@ -283,5 +334,16 @@ public class gameView extends JFrame {
 			
 		}
 		
+	}
+	
+	private Image getScaledImage(Image srcImg, int w, int h){
+	    BufferedImage resizedImg = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
+	    Graphics2D g2 = resizedImg.createGraphics();
+
+	    g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+	    g2.drawImage(srcImg, 0, 0, w, h, null);
+	    g2.dispose();
+
+	    return resizedImg;
 	}
 }
